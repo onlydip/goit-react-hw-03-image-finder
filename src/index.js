d38a15c1e4723a4b59b91347fc066af5cf7383f0
Aleksandr Harianin <onlydipro@gmail.com>
2023-09-15 13:08:04 +0300

    11

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Global, ThemeProvider } from '@emotion/react';
-import 'modern-normalize';
-
-import { App } from 'components';
-import { GlobalStyles, theme } from 'styles';
+import { App } from 'components/App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyles} />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
